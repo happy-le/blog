95155d6b8164df032b7cf44f7938b4983beb17d3
xule <xule@ikingtec.com>
2021-01-28 16:58:34 +0800

    init

---
+++ blog1/bin/www.js
@@ -0,0 +1,8 @@
+const http = require("http")
+
+const PORT = 8000
+const serverHandle = require("../app")
+
+const server = http.createServer(serverHandle)
+server.listen(PORT)
+console.log("node服务启动成功>>>>>>>>>>>")
---
+++ blog1/src/router/blog.js
@@ -0,0 +1,44 @@
+const { getList, getDetail, newBlog } = require("../controller/blog");
+const { SuccessModel, ErrorModel } = require("../model/resModel");
+
+const handleBlogRouter = (req, res) => {
+  const method = req.method;
+
+  // 获取博客列表
+  if (method === "GET" && req.path === "/api/blog/list") {
+    const author = req.query.author || "";
+    const keyword = req.query.keyword || "";
+    const listDate = getList(author, keyword);
+    return new SuccessModel(listDate);
+  }
+
+  // 获取博客详情
+  if (method === "GET" && req.path === "/api/blog/detail") {
+    const id = req.query.id || "";
+    const data = getDetail(id);
+    return new SuccessModel(data);
+  }
+
+  // 创建博客
+  if (method === "POST" && req.path === "/api/blog/new") {
+    console.log(req.body)
+    const data = newBlog(req.body);
+    return new SuccessModel(data);
+  }
+
+  // 更新博客
+  if (method === "POST" && req.path === "/api/blog/update") {
+    return {
+      msg: "更新博客",
+    };
+  }
+
+  // 删除博客
+  if (method === "POST" && req.path === "/api/blog/delete") {
+    return {
+      msg: "删除博客",
+    };
+  }
+};
+
+module.exports = handleBlogRouter;
